510987a6d07698120076fd58a8b2a42728aff54a
Aadityaryal <aadityaryal8@gmail.com>
2026-01-30 01:10:00 +0545

    Mount admin routes in app

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { connectDatabase } from './database/mongodb';
 import { PORT } from './config';
 import cors from 'cors';
 import authRoutes from "./routes/auth.route";
+import adminRoutes from "./routes/admin.route";
 
 const app: Application = express();
 
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
 app.get('/', (req: Request, res: Response) => {
     return res.status(200).json({ success: "true", message: "Welcome to the API" });
 });
